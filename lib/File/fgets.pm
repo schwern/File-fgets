6fa17315bc85a48be75d3fa0c0b7bb7f08e1f3ad
Michael G. Schwern <schwern@pobox.com>
2010-05-28 15:28:55 -0700

    Version 0.0.3

--- lib/File/fgets.pm
+++ lib/File/fgets.pm
@@ -3,7 +3,7 @@ package File::fgets;
 use strict;
 use warnings;
 
-use version; our $VERSION = qv("v0.0.2");
+use version; our $VERSION = qv("v0.0.3");
 
 use XSLoader;
 XSLoader::load __PACKAGE__, $VERSION;
